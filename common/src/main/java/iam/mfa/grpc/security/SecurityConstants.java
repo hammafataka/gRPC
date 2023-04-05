92aec2a65b355ccff20ce1f8816264cab3c75795
FATAKA <mfataka@monetplus.cz>
2023-04-06 00:19:57 +0200

    add default value for clinet_id_context_key

--- common/src/main/java/iam/mfa/grpc/security/SecurityConstants.java
+++ common/src/main/java/iam/mfa/grpc/security/SecurityConstants.java
@@ -17,6 +17,6 @@ public class SecurityConstants {
     public static final String BEARER_TYPE = "Bearer";
 
     public static final Metadata.Key<String> AUTHORIZATION_METADATA_KEY = Metadata.Key.of("Authorization", ASCII_STRING_MARSHALLER);
-    public static final Context.Key<String> CLIENT_ID_CONTEXT_KEY = Context.key("clientId");
+    public static final Context.Key<String> CLIENT_ID_CONTEXT_KEY = Context.keyWithDefault("clientId", "Not found");
 
 }
